158eeb31b0c867518bb87d08bb8d6019b52d2f44
tudeptrai <anhtunguyen643@gmail.com>
2025-05-28 04:14:11 +0700

    Build Layout FE(next)

---
+++ frontend/src/layouts/ProfileLayout/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+
+
+const cx = classNames.bind(styles);
+
+function Sidebar() {
+    return ( <div></div> );
+}
+
+export default Sidebar;

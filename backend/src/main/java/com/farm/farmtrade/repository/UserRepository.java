a0f1d7de0f59243ed1dacff59c37a7c4664ec687
tudeptrai <anhtunguyen643@gmail.com>
2025-05-28 13:59:02 +0700

    fix bug

--- backend/src/main/java/com/farm/farmtrade/repository/UserRepository.java
+++ backend/src/main/java/com/farm/farmtrade/repository/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     boolean existsByUsername(String username);
     Optional<User> findByUsername(String username);
 //    boolean existsByEmail(String email);

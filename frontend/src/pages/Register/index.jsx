1b4ee344c1f9930c7a2945e62149b45adeb2dcf3
tudeptrai <anhtunguyen643@gmail.com>
2025-06-02 03:16:53 +0700

    Continue build fe

--- frontend/src/pages/Register/index.jsx
+++ frontend/src/pages/Register/index.jsx
@@ -7,22 +7,8 @@ import styles from "./Register.module.scss";
 const cx = classNames.bind(styles);
 
 const Register = () => {
-  const [step, setStep] = useState(1);
   const [role, setRole] = useState("");
   const navigate = useNavigate();
-
-  const roles = ["Supplier", "Customer", "Shipper"];
-
-  const handleRoleSelect = (role) => {
-
-    setFormData((prev) => ({
-      ...prev,
-      role: role,
-    }));
-
-    setRole(role);
-    setStep(2);
-  };
 
   const [formData, setFormData] = useState({
     fullName: "",
@@ -35,10 +21,11 @@ const Register = () => {
     businessName: "",
     certification: "",
     address: "",
-    licensePlate: "",
-    vehicle: "",
-
   });
+
+  const handleRoleSelect = (role) => {
+    setRole(role.target.value);
+  };
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -65,106 +52,96 @@ const Register = () => {
 
   return (
     <div className="container mt-3" style={{ maxWidth: "500px" }}>
-      {step === 1 && (
-        <h3 className={cx(styles.title, "text-center")}>Choose role</h3>
-      )}
+      <h3 className={cx(styles.title, "text-center")}>Register</h3>
 
-      {step === 1 && (
-        <div className={cx(styles["form-role"])}>
-          {roles.map((role) => (
-            <div
-              key={role}
-              onClick={() => handleRoleSelect(role)}
-              className={styles["role-item"]}
-            >
-              <img
-                src="/role.png" // đổi thành ảnh phù hợp
-                alt={role}
-                width="100"
-              />
-              <p>{role}</p>
-            </div>
-          ))}
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label>Full Name</label>
+          <input
+            type="text"
+            className="form-control"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleChange}
+            placeholder="Your Full Name"
+            required
+          />
         </div>
-      )}
-      {step === 2 && (
-        <h3 className={cx(styles.title, "text-center")}>Register for {role}</h3>
-      )}
-      {step === 2 && (
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label>Full Name</label>
-            <input
-              type="text"
-              className="form-control"
-              name="fullName"
-              value={formData.fullName}
-              onChange={handleChange}
-              placeholder="Your Full Name"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label>Username</label>
-            <input
-              type="text"
-              className="form-control"
-              name="username"
-              value={formData.username}
-              onChange={handleChange}
-              placeholder="Your Username"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label>Email</label>
-            <input
-              type="email"
-              className="form-control"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Your Email"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label>Mật khẩu</label>
-            <input
-              type="password"
-              className="form-control"
-              name="passwordHash"
-              value={formData.passwordHash}
-              onChange={handleChange}
-              placeholder="Your Password"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label>Nhập lại mật khẩu</label>
-            <input
-              type="password"
-              className="form-control"
-              name="confirmPassword"
-              value={formData.confirmPassword}
-              onChange={handleChange}
-              placeholder="Your Password"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label>Phone</label>
-            <input
-              type="tel"
-              className="form-control"
-              name="phone"
-              value={formData.phone}
-              onChange={handleChange}
-              placeholder="Your phone"
-              required
-            />
-          </div>
-          {role === "Supplier" && (
+        <div className="mb-3">
+          <label>Username</label>
+          <input
+            type="text"
+            className="form-control"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            placeholder="Your Username"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label>Email</label>
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Your Email"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label>Mật khẩu</label>
+          <input
+            type="password"
+            className="form-control"
+            name="passwordHash"
+            value={formData.passwordHash}
+            onChange={handleChange}
+            placeholder="Your Password"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label>Nhập lại mật khẩu</label>
+          <input
+            type="password"
+            className="form-control"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            placeholder="Your Password"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label>Phone</label>
+          <input
+            type="tel"
+            className="form-control"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            placeholder="Your phone"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Chọn vai trò</label>
+          <select
+            className="form-select"
+            value={role}
+            onChange={handleRoleSelect}
+            required
+          >
+            <option value="">-- Select Role --</option>
+            <option value="Customer">Customer</option>
+            <option value="Supplier">Supplier</option>
+          </select>
+        </div>
+        {role === "Supplier" && (
+          <>
             <div className="mb-3">
               <label>Business Name</label>
               <input
@@ -177,8 +154,6 @@ const Register = () => {
                 required
               />
             </div>
-          )}
-          {role === "Supplier" && (
             <div className="mb-3">
               <label>Certification</label>
               <input
@@ -191,54 +166,69 @@ const Register = () => {
                 required
               />
             </div>
-          )}
-          {role === "Customer" && (
-            <div className="mb-3">
-              <label>Address</label>
-              <input
-                type="text"
-                className="form-control"
-                name="address"
-                value={formData.address}
-                onChange={handleChange}
-                placeholder="Your Address"
-                required
-              />
-            </div>
-          )}
-          {role === "Shipper" && (
-            <div className="mb-3">
-              <label>License Plate</label>
-              <input
-                type="text"
-                className="form-control"
-                name="licensePlate"
-                value={formData.licensePlate}
-                onChange={handleChange}
-                placeholder="Your License Plate"
-                required
-              />
-            </div>
-          )}
-          {role === "Shipper" && (
-            <div className="mb-3">
-              <label>Vehicle</label>
-              <input
-                type="text"
-                className="form-control"
-                name="vehicle"
-                value={formData.vehicle}
-                onChange={handleChange}
-                placeholder="Your Vehicle"
-                required
-              />
-            </div>
-          )}
-          <button type="submit" className="btn btn-success w-100">
-            Đăng ký
-          </button>
-        </form>
-      )}
+          </>
+        )}
+        {role === "Customer" && (
+          <div className="mb-3">
+            <label>Address</label>
+            <input
+              type="text"
+              className="form-control"
+              name="address"
+              value={formData.address}
+              onChange={handleChange}
+              placeholder="Your Address"
+              required
+            />
+          </div>
+        )}
+        <button type="submit" className="btn btn-success w-100">
+          Đăng ký
+        </button>
+      </form>
+      <div class="my-4 d-flex align-items-center text-center">
+        <span class="w-100 border-bottom border-secondary"></span>
+        <span class="mx-2 text-uppercase small">Or</span>
+        <span class="w-100 border-bottom border-secondary"></span>
+      </div>
+      <div class="d-flex flex-column flex-sm-row justify-content-center">
+        <a
+          href="http://localhost:8080/farmtrade/oauth2/authorization/google"
+          class="mr-4 mb-3 py-3 btn btn-light w-100 d-flex align-items-center justify-content-center shadow-sm me-4"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="30"
+            height="30"
+            viewBox="0 0 48 48"
+          >
+            <path
+              fill="#FFC107"
+              d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+            ></path>
+            <path
+              fill="#FF3D00"
+              d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+            ></path>
+            <path
+              fill="#4CAF50"
+              d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+            ></path>
+            <path
+              fill="#1976D2"
+              d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+            ></path>
+          </svg>
+        </a>
+      </div>
+      <p class="mt-4 text-center">
+        You have an account?
+        <a class="text-primary" href="login.html">
+          Login
+        </a>
+      </p>
     </div>
   );
 };

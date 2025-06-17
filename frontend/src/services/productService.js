bf92395cc65ed54f569f20aa23105fe4f54343c3
thuhataplamdev <thuha140105@gmail.com>
2025-06-17 23:40:09 +0700

    Update frontend page search result

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,64 +1,64 @@
-import { request, formRequest } from "../utils/httpRequest";
+import {request, formRequest} from "../utils/httpRequest";
 
 export const addProduct = async (data) => {
-  try {
-    const response = await formRequest.post("/products", data);
-    return response.data;
-  } catch (error) {
-    throw error.response?.data || error.message;
-  }
+    try {
+        const response = await formRequest.post("/products", data);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
 };
 
 export const renderProduct = async () => {
-  try {
-    const response = await request.get("/products");
-    return response.data;
-  } catch (error) {
-    throw error.response?.data || error.message;
-  }
+    try {
+        const response = await request.get("/products");
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
 };
 
 export const renderProductById = async (id) => {
-  try {
-    const response = await request.get(`/products/${id}`);
-    return response.data;
-  } catch (error) {
-    throw error.response?.data || error.message;
-  }
+    try {
+        const response = await request.get(`/products/${id}`);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
 };
 
 export const renderProductBySupplierId = async (id, sortBy, sortDir, page, size) => {
-  try {
-    const response = await request.get(`/products/supplier/${id}`, {
-      params: {
-        sortBy,
-        sortDir,
-        page,
-        size,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    throw error.response?.data || error.message;
-  }
+    try {
+        const response = await request.get(`/products/supplier/${id}`, {
+            params: {
+                sortBy,
+                sortDir,
+                page,
+                size,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
 };
 
 export const deleteProduct = async (id) => {
-  try {
-    const response = await request.delete(`/products/${id}`);
-    return response.data;
-  } catch (error) {
-    throw error.response?.data || error.message;
-  }
+    try {
+        const response = await request.delete(`/products/${id}`);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
 };
 
-export const searchProducts = async (keyword) => {
-  try {
-    const response = await request.get("/products/search", {
-      params: { keyword },
-    });
-    return response.data;
-  } catch (error) {
-    throw error.response?.data || error.message;
-  }
+export const searchProducts = async (keyword, category, rating) => {
+    try {
+        const response = await request.get("/products/search", {
+            params: {keyword, category, rating},
+        });
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
 };

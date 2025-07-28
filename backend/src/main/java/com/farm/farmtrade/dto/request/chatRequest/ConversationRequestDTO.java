4b2acfccb1efc0eebc4c62431beee7609541363c
tudeptrai <anhtunguyen643@gmail.com>
2025-07-29 05:26:47 +0700

    refactor: refactor count notifications

--- backend/src/main/java/com/farm/farmtrade/dto/request/chatRequest/ConversationRequestDTO.java
+++ backend/src/main/java/com/farm/farmtrade/dto/request/chatRequest/ConversationRequestDTO.java
@@ -1,8 +1,13 @@
 package com.farm.farmtrade.dto.request.chatRequest;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ConversationRequestDTO {
     @NotEmpty(message = "userIds cannot be empty")
     private List<Integer> userIds;
@@ -34,5 +39,57 @@ public class ConversationRequestDTO {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public static class ProductMessageRequest {
+        private Integer senderId;
+        private Integer productId;
+
+        public Integer getSenderId() {
+            return senderId;
+        }
+
+        public void setSenderId(Integer senderId) {
+            this.senderId = senderId;
+        }
+
+        public Integer getProductId() {
+            return productId;
+        }
+
+        public void setProductId(Integer productId) {
+            this.productId = productId;
+        }
+
+        class ConversationRequest {
+            private List<Integer> userIDs;
+            private boolean isGroup;
+            private String name;
+
+            // Getters and setters
+            public List<Integer> getUserIDs() {
+                return userIDs;
+            }
+
+            public void setUserIDs(List<Integer> userIDs) {
+                this.userIDs = userIDs;
+            }
+
+            public boolean isGroup() {
+                return isGroup;
+            }
+
+            public void setGroup(boolean isGroup) {
+                this.isGroup = isGroup;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+        }
     }
 }
